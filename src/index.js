7c021f9575d60b28c4773c6ccb30383fd2a07f1f
parkjeyoung <dkfmwpsxm@naver.com>
2022-10-03 17:00:55 +0900

    Feat : path 방식 및 pop / push state 이벤트 추가

--- src/index.js
+++ src/index.js
@@ -4,16 +4,33 @@ const root = document.getElementById('App');
 const navigation = document.getElementById('navigation');
 
 const routes = [
-  {path: '', component: ProductListPage},
-  {path: 'cart', component: CartPage},
-  {path: 'detail', component: ProductDetailPage},
+  {path: '/', component: ProductListPage},
+  {path: '/cart', component: CartPage},
+  {path: '/detail', component: ProductDetailPage},
 ];
 
-const render = () => {
-  const hash = window.location.hash.replace('#', '');
-  const component = routes.find(route => route.path === hash).component || NotFound;
+const render = path => {
+  const _path = path ?? window.location.pathname;
+  const component = routes.find(route => route.path === _path).component || NotFound;
   root.replaceChildren(new component(1));
 };
 
-window.addEventListener('hashchange', render);
-window.addEventListener('DOMContentLoaded', render);
+navigation.addEventListener('click', e => {
+  if (!e.target.matches('#navigation > li > a')) return;
+  e.preventDefault();
+
+  const path = e.target.getAttribute('href');
+
+  if (path === window.location.pathname) return;
+
+  window.history.pushState(null, null, path);
+  render(path);
+});
+
+window.addEventListener('popstate', () => {
+  console.log('popstate:', window.location.pathname);
+  render();
+});
+window.addEventListener('DOMContentLoaded', () => {
+  render();
+});
